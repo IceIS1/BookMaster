c6b83df53530dd15fe32c4e4ba8e83af352818b0
Sigmar Arason <sa1.h18@nemandi.ntv.is>
2018-11-06 23:37:52 +0000

    hello Tóti

--- BookMaster/Program.cs
+++ BookMaster/Program.cs
@@ -15,7 +15,7 @@ namespace BookMaster
             do
             {
                 Console.WriteLine(" | C: Categories | B: Books | Q: Quit ");
-                Console.WriteLine(" Hello simmi ");
+                Console.WriteLine(" Hello Tóti ");
                 ConsoleKeyInfo key = Console.ReadKey();
                 Console.Clear();
                 switch (key.Key)
