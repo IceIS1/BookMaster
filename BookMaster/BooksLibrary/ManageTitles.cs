adc36166a3541f5c7bab53b6604667c5855b32fe
Sigmar Arason <sa1.h18@nemandi.ntv.is>
2018-11-11 17:28:16 +0000

    NugetMenu

---
+++ BookMaster/BooksLibrary/ManageTitles.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMaster.BooksLibrary
+{
+    class ManageTitles
+    {
+        public static void Add()
+        {
+            Console.WriteLine("Add Title not implemented");
+            Console.ReadKey();
+        }
+        public static void Edit()
+        {
+            Console.WriteLine("Edit Title not implemented");
+            Console.ReadKey();
+        }
+        public static void Delete()
+        {
+            Console.WriteLine("Delete Title not implemented");
+            Console.ReadKey();
+        }
+        public static void List()
+        {
+            Console.WriteLine("List Title not implemented");
+            Console.ReadKey();
+        }
+    }
+}
