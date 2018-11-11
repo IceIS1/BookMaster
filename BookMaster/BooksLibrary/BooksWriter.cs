ed45d01e4243d26a1d5ae0952b0772fbec39a715
Sigmar Arason <sa1.h18@nemandi.ntv.is>
2018-11-11 18:07:42 +0000

    samsetning

--- BookMaster/BooksLibrary/BooksWriter.cs
+++ BookMaster/BooksLibrary/BooksWriter.cs
@@ -10,7 +10,7 @@ using System.Xml.Serialization;
 using System.Collections;
 
 
-namespace BookMaster
+namespace BookMaster.BooksLibrary
 {
     class BooksWriter
     {
--- BookMaster/BooksLibrary/ManageTitles.cs
+++ BookMaster/BooksLibrary/ManageTitles.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookMaster.BooksLibrary;
 
 namespace BookMaster.BooksLibrary
 {
@@ -10,6 +11,7 @@ namespace BookMaster.BooksLibrary
     {
         public static void Add()
         {
+            AddNewBook();
             Console.WriteLine("Add Title not implemented");
             Console.ReadKey();
         }
@@ -25,8 +27,61 @@ namespace BookMaster.BooksLibrary
         }
         public static void List()
         {
+            ListAllBooks();
             Console.WriteLine("List Title not implemented");
             Console.ReadKey();
         }
+
+        public static void ListAllBooks()
+        {
+
+            // BooksWriter bookwr = new BooksWriter();
+            //  bookwr.AddBook();
+            // BooksWriter.AddBook(1, "Sigmar", "Cat world2");
+            // Console.WriteLine("not implemented");
+            BooksWriter.ListBooks();
+        }
+
+        public static void AddNewBook()
+        {
+            Console.WriteLine("ISBN number:");
+            int isbnNr = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Author name:");
+            String AuthorName = Console.ReadLine();
+            Console.WriteLine("Book title:");
+            String BookTitle = Console.ReadLine();
+
+            BooksWriter.AddBook(isbnNr, AuthorName, BookTitle);
+
+            Console.Clear();
+            //   BooksWriter.AddBook(1, "Sigmar", "Cat world2");
+            /* bool keepRunning = true;
+             do
+             {
+                 Console.WriteLine(" | Q: Cancel ");
+
+
+
+                 ConsoleKeyInfo key = Console.ReadKey();
+                 Console.Clear();
+                 switch (key.Key)
+                 {
+
+                 //    case ConsoleKey.S:
+                   //      Console.WriteLine("not implemented");
+                     //    break;
+
+                     case ConsoleKey.Escape:
+                         keepRunning = false;
+                         break;
+                     default:
+                         Console.WriteLine(key);
+                         break;
+                 }
+             } while (keepRunning); */
+
+        }
+
+
     }
 }
