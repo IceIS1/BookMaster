f35c1ff2838d2581eed6483fd37c13937ea518c9
Þórarinn <rarinn@ZATHOG-WS>
2018-11-13 22:26:14 +0000

    Litir í console

--- BookMaster/Startup/NugetMenu.cs
+++ BookMaster/Startup/NugetMenu.cs
@@ -7,6 +7,7 @@ namespace BookMaster.Startup
 {
     class NugetMenu
     {
+       
         public static void InitMenu()
        
              {
@@ -16,7 +17,8 @@ namespace BookMaster.Startup
                   .Add("Delete", () => ManageGeneres.Delete())
                   .Add("List", () => ManageGeneres.List())
                   .Add("Close Generes", ConsoleMenu.Close)
-                  .Add("Exit App", () => Environment.Exit(0));
+                  .Add("Exit App", () => Environment.Exit(0))
+                    .Configure(config => { config.SelectedItemBackgroundColor = ConsoleColor.Blue; config.SelectedItemForegroundColor = ConsoleColor.Green; } ); ;
 
                 var titlesMainmenu = new ConsoleMenu()
                   .Add("Add", () => ManageTitles.Add())
@@ -24,18 +26,20 @@ namespace BookMaster.Startup
                   .Add("Delete", () => ManageTitles.Delete())
                   .Add("List", () => ManageTitles.List())
                   .Add("Close Titles", ConsoleMenu.Close)
-                  .Add("Exit App", () => Environment.Exit(0));
+                  .Add("Exit App", () => Environment.Exit(0))
+                  .Configure(config => { config.Selector = "--> "; config.SelectedItemBackgroundColor = ConsoleColor.Blue; config.SelectedItemForegroundColor = ConsoleColor.Green; });
 
                 var BookManagerMainmenu = new ConsoleMenu()
                   .Add("Generes", () => generesMainmenu.Show())
                   .Add("Titles", () => titlesMainmenu.Show())
                   .Add("Close Bookmanager", ConsoleMenu.Close)
-                  .Add("Exit App", () => Environment.Exit(0));
+                  .Add("Exit App", () => Environment.Exit(0))
+                  .Configure(config => { config.Selector = "--> "; config.SelectedItemBackgroundColor = ConsoleColor.Blue; config.SelectedItemForegroundColor = ConsoleColor.Green; });
 
                 var menu = new ConsoleMenu()
                   .Add("Bookmanager", () => BookManagerMainmenu.Show())  
                   .Add("Exit", () => Environment.Exit(0))
-                  .Configure(config => { config.Selector = "--> "; });
+                  .Configure(config => { config.Selector = "--> "; config.SelectedItemBackgroundColor = ConsoleColor.Blue; config.SelectedItemForegroundColor = ConsoleColor.Green; });
 
                 menu.Show();
             }
